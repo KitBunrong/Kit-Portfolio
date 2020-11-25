0aa3919e3fcf235664883d25105466279465bd83
KitBunrong <garyswartz18@gmail.com>
2020-11-25 17:00:20 +0700

    complete kit fronted

---
+++ layouts/default.vue
@@ -0,0 +1,113 @@
+<template>
+  <v-app dark>
+    <v-app-bar hide-on-scroll fixed flat app color="#272727">   
+      <v-app-bar-nav-icon class="d-block d-md-none"></v-app-bar-nav-icon>
+      <v-toolbar-title class="text-button text-uppercase">kbr3, the creator!</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <div class="app-search" v-click-outside="onClickOutside"
+            @click="active = true"
+            :class="active ? 'app-search' : 'collapse'">
+        <div class="search-wrapper">
+          <v-text-field 
+            hide-details="auto" 
+            dense 
+            single-line 
+            outlined 
+            rounded 
+            label="Searching for something"
+            append-icon="mdi-magnify"
+            color="white"
+          >
+          </v-text-field>
+        </div>
+      </div>
+    </v-app-bar>
+
+    <v-main>
+      <nuxt />
+    </v-main>
+
+    <v-footer class="grey--text text--darken-3 pt-8 grey lighten-3" inset>
+      <v-container>
+        <v-row no-gutters>
+          <v-col class="text-body-2 font-italic mb-md-0 mb-6 col-md-6 col-12">
+            Frontend Dev since 2018
+            <br>
+            Based in Phnom Penh moved from Siem Reap
+            <br>
+            Currently working full-time as a fullstack developer on both Earth and Mars.
+          </v-col>
+
+          <v-col class="text-sm-right text-left col-md-6 col-12">
+            <v-btn class="grey--text text--darken-3" rounded text small fab href="https://www.facebook.com/kit.bunrong.1" target="_blank">
+              <v-icon class="mdi mdi-facebook" style="font-size: 22px"></v-icon>
+            </v-btn>
+            <v-btn class="grey--text text--darken-3" rounded text small fab href="https://www.instagram.com/bunrongkit/" target="_blank">
+              <v-icon class="mdi mdi-instagram" style="font-size: 22px"></v-icon>
+            </v-btn>
+            <v-btn class="grey--text text--darken-3" rounded text small fab href="https://twitter.com/KitBunrong" target="_blank">
+              <v-icon class="mdi mdi-twitter" style="font-size: 22px"></v-icon>
+            </v-btn>
+            <v-btn class="grey--text text--darken-3" rounded text small fab href="https://kh.linkedin.com/in/kit-bunrong-1b8126193?trk=public_profile_samename-profile_profile-result-card_result-card_full-click" target="_blank">
+              <v-icon class="mdi mdi-linkedin" style="font-size: 22px"></v-icon>
+            </v-btn>
+          </v-col>
+
+          <v-row class="text-caption mt-6 no-gutters">
+            <v-col class="col-md-6 col-12">
+              Website made with 
+              <a href="https://nuxtjs.org/" target="_blank">nuxtjs</a>
+              &
+              <a href="https://vuetifyjs.com/" target="_blank">vuetifyjs</a>
+              , api driven using 
+              <a href="https://ghost.org/" target="_blank">ghost.</a>
+            </v-col>
+            <v-col class="col-md-6 col-12 text-right">
+              <span>KBR3, THE CREATOR! 2018-21©</span>
+            </v-col>
+          </v-row>
+        </v-row>
+      </v-container>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  data: () => ({
+    active: false,
+  }),
+  methods: {
+      onClickOutside () {
+        this.active = false
+      },
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+
+.app-search{
+  transition: width .5s;
+  width: 100%;
+}
+.collapse{
+  width: 40%;
+}
+.search-wrapper{
+  position: relative;
+}
+
+@media (min-width: 600px){
+  .app-search{
+    width: 30%
+  }
+}
+@media (min-width: 600px){
+  .collapse{
+    width: 18%;
+  }
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,334 @@
+<template>
+  <div>
+    <!-- Particles Home Page -->
+    <div class="particles-wrapper particles">
+      <vue-particles
+        color="#ffffff"
+        :particleOpacity="0.7"
+        :particlesNumber="120"
+        shapeType="circle"
+        :particleSize="4"
+        linesColor="#cccccc"
+        :linesWidth="1"
+        :lineLinked="true"
+        :lineOpacity="0.4"
+        :linesDistance="150"
+        :moveSpeed="4"
+        :hoverEffect="true"
+        hoverMode="grab"
+        :clickEffect="true"
+        clickMode="push"
+      >
+      </vue-particles>
+
+      <div class="content pa-sm-8 pa-4 d-flex align-center justify-sm-space-between justify-center flex-sm-row flex-column">
+        <v-card class="mt-sm-0 transparent" text rounded="0">
+          <v-card-title>
+            <h1 class="text-xl-h1 text-lg-h2 text-sm-h3 text-h4 title">
+              <span style="font-family: 'Merienda', cursive;">This is</span>
+              <span style="font-family: 'Merienda', cursive;">Bunrong KIT</span>
+              <v-card-subtitle class="text-overline pa-0 text-right mr-1 mt-2 primary--text">
+                <span>¯\_(ツ)_/¯ aka (gary)</span>
+              </v-card-subtitle> 
+            </h1>
+          </v-card-title>
+          <v-card-subtitle class="text-overline subtitle">
+            <span>A full stack web developer</span>
+          </v-card-subtitle>
+          <v-card-text class="text-body-1 subtitle">
+            Click
+            <a href="#">here</a>
+            to view my work!
+          </v-card-text>
+        </v-card>
+        <v-btn href="https://github.com/KitBunrong" target="_blank" class="kit-github mt-sm-0" x-small text>connect with kit</v-btn>
+      </div>
+    </div>
+
+    <!-- Skills -->
+    <div class="black pt-6">
+      <v-container>
+        <h3 class="text-h5 text-uppercase">Skills</h3>
+      </v-container>
+      <v-container>
+        <v-row>
+
+          <v-col class="col-12">
+            <h4 class="text-h6 text-uppercase text-center mb-4">frontend</h4>
+            <v-chip-group class="mb-4" column>
+              <v-tooltip top color="black" v-for="(frontend, i) in frontends" :key="i">
+                <template #activator="{ on }">
+                  <v-chip 
+                    class="gray darken-3 aos-init aos-animate" 
+                    :href="frontend.to" 
+                    :data-aos-delay="i*100" data-aos="fade-up"
+                    v-on="on"  
+                    label
+                  > 
+                    <v-icon :class="frontend.icons" left></v-icon>
+                    {{frontend.name}} 
+                  </v-chip>
+                </template>
+                <span>{{frontend.tooltip}}</span>
+              </v-tooltip>
+            </v-chip-group>
+          </v-col>
+          
+          <v-col class="col-12">
+            <h4 class="text-h6 text-uppercase text-center mb-4">backend</h4>
+            <v-chip-group class="mb-4" column>
+              <v-tooltip top color="black" v-for="(backend, i) in backends" :key="i">
+                <template #activator="{ on }">
+                  <v-chip class="gray darken-3 aos-init aos-animate" :data-aos-delay="i*100" data-aos="fade-up" v-on="on"  :href="backend.to" label> 
+                    <v-icon :class="backend.icons" left></v-icon>
+                    {{backend.name}} 
+                  </v-chip>
+                </template>
+                <span>{{backend.tooltip}}</span>
+              </v-tooltip>
+            </v-chip-group>
+          </v-col>
+
+          <v-col class="col-12">
+            <h4 class="text-h6 text-uppercase text-center mb-4">tools</h4>
+            <v-chip-group class="mb-4" column>
+              <v-tooltip top color="black" v-for="(tool, i) in tools" :key="i">
+                <template #activator="{ on }">
+                  <v-chip class="gray darken-3 aos-init aos-animate" :data-aos-delay="i*100" data-aos="fade-up" v-on="on"  :href="tool.to" label> 
+                    <v-icon :class="tool.icons" left></v-icon>
+                    {{tool.name}} 
+                  </v-chip>
+                </template>
+                <span>{{tool.tooltip}}</span>
+              </v-tooltip>
+            </v-chip-group>
+          </v-col>
+
+          <v-col class="col-12">
+            <h4 class="text-h6 text-uppercase text-center mb-4">cms</h4>
+            <v-chip-group class="mb-4" column>
+              <v-tooltip top color="black" v-for="(cms, i) in cmss" :key="i">
+                <template #activator="{ on }">
+                  <v-chip class="gray darken-3 aos-init aos-animate" :data-aos-delay="i*100" data-aos="fade-up" v-on="on"  :href="cms.to" label> 
+                    <v-icon :class="cms.icons" left></v-icon>
+                    {{cms.name}} 
+                  </v-chip>
+                </template>
+                <span>{{cms.tooltip}}</span>
+              </v-tooltip>
+            </v-chip-group>
+          </v-col>
+        </v-row>
+      </v-container>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      frontends:[
+        {
+          to: 'https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5',
+          icons: 'mdi mdi-language-html5',
+          name: "HTML",
+          tooltip: 'The bare bone of Webpage'
+        },
+        {
+          to: 'https://developer.mozilla.org/en-US/docs/Web/CSS',
+          icons: 'mdi mdi-language-css3',
+          name: "CSS3",
+          tooltip: 'The skin of the Webpage'
+        },
+        {
+          to: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+          icons: 'mdi mdi-language-javascript',
+          name: "JavaScript",
+          tooltip: 'The hearth of Javascript'
+        },
+        {
+          to: 'https://vuejs.org/',
+          icons: 'mdi mdi-vuejs',
+          name: "VueJS",
+          tooltip: 'Progressive Javascript Framework'
+        },
+        {
+          to: 'https://en.wikipedia.org/wiki/Representational_state_transfer',
+          icons: '',
+          name: "REST API",
+          tooltip: 'Application Programming language(API)'
+        },
+        {
+          to: 'https://sass-lang.com/',
+          icons: 'mdi mdi-sass',
+          name: "Sass",
+          tooltip: 'Stylesheet language compiled to CSS'
+        },
+        {
+          to: 'https://nuxtjs.org/',
+          icons: 'mdi mdi-nuxt',
+          name: "NuxtJS",
+          tooltip: 'Framework using Vue'
+        },
+        {
+          to: 'https://en.wikipedia.org/wiki/Responsive_web_design',
+          icons: 'mdi mdi-responsive',
+          name: "Responsive",
+          tooltip: 'Design Web layouts for differnt screen size'
+        },
+        {
+          to: 'https://getbootstrap.com/',
+          icons: 'mdi mdi-bootstrap',
+          name: "BootStrap",
+          tooltip: 'CSS Framework'
+        },
+        {
+          to: 'https://tailwindcss.com/',
+          icons: 'mdi mdi-tailwind',
+          name: "TailwindCSS",
+          tooltip: 'CSS Framework'
+        },
+      ],
+      backends: [
+        {
+          to: 'https://www.mysql.com/',
+          icons: 'mdi mdi-database-search',
+          name: "MYSQL",
+          tooltip: 'Open-source database management system'
+        },
+        {
+          to: 'https://www.php.net/',
+          icons: 'mdi mdi-language-php',
+          name: "PHP",
+          tooltip: 'Side-server scripting language'
+        },
+        {
+          to: 'https://redis.io/',
+          icons: '',
+          name: 'Redis',
+          tooltip: 'In-memory data structure'
+        },
+        {
+          to: 'https://www.rabbitmq.com/',
+          icons: '',
+          name: 'RabbitMQ',
+          tooltip: 'Message-broker'
+        },
+      ],
+      tools: [
+        {
+          to: 'https://git-scm.com/',
+          icons: 'mdi mdi-git',
+          name: 'Git',
+          tooltip: 'Source-version control system'
+        },
+        {
+          to: 'https://www.docker.com/',
+          icons: 'mdi mdi-docker',
+          name: 'Docker',
+          tooltip: 'Containerized applications'
+        },
+        {
+          to: 'https://www.nginx.com/',
+          icons: '',
+          name: 'Nginx',
+          tooltip: 'Web Server'
+        },
+        {
+          to: 'https://www.apache.org/',
+          icons: '',
+          name: 'Apache',
+          tooltip: 'Web Server'
+        },
+        {
+          to: 'https://www.npmjs.com/',
+          icons: 'mdi mdi-npm',
+          name: 'NPM',
+          tooltip: 'Node package manager'
+        },
+        {
+          to: 'https://yarnpkg.com/',
+          icons: '',
+          name: 'Yarn',
+          tooltip: 'Node package manager'
+        },
+      ],
+      cmss:[
+        {
+          to: 'https://wordpress.org/',
+          icons: 'mdi mdi-wordpress',
+          name: 'WordPress',
+          tooltip: 'Popular open-source CMS'
+        },
+        {
+          to: 'https://ghost.org/',
+          icons: '',
+          name: 'Ghost',
+          tooltip: 'Open-source CMS using Node.js'
+        },
+        {
+          to: 'https://directus.io/',
+          icons: '',
+          name: 'Directus',
+          tooltip: 'Open-source headless CMS'
+        },
+        {
+          to: 'https://getcockpit.com/',
+          icons: '',
+          name: 'Cockpit',
+          tooltip: 'Open-source headless CMS'
+        },
+      ],
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style lang="scss" >
+@import url('https://fonts.googleapis.com/css2?family=Merienda:wght@700&family=Patrick+Hand&display=swap');
+
+.particles {
+  width: 100%;
+  height: 100vh;
+  background: linear-gradient(180deg,#000,#000 44%,transparent);
+  margin-bottom: 40px;
+}
+
+#particles-js{
+  height: 100%;
+}
+
+.particles-wrapper {
+  position: relative;
+}
+
+.content{
+  pointer-events: none;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.particles-wrapper .content a {
+    pointer-events: auto;
+}
+
+.kit-github{
+  align-self: flex-end;
+}
+
+.v-slide-group__content {
+    justify-content: center;
+}
+
+</style>
